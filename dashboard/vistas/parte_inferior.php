4cab5cc29a99a3c651cdebb7cea0d888b1870b77
Rolando Picon Nadales <102068223+EngineerPicon@users.noreply.github.com>
2022-07-22 04:52:32 -0400

    Add files via upload

---
+++ dashboard/vistas/parte_inferior.php
@@ -0,0 +1,73 @@
+
+      </div>
+      <!-- End of Main Content  -->
+
+      <!-- Footer (inicio de pie de página)-->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span><p><address>Desarrollado por: ANA ANGEL - © Todos los derechos reservados 2022</address></p></span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer (fin de pie de página)-->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal (cuadro de dialogo para cerrar sesión) -->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">¿Desea salir y cerrar Sesión?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body"></div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button> <!-- Botón de cancelar -->
+          <a class="btn btn-primary" href="../bd/logout.php">Salir</a> <!-- Botón para salir -->
+  
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+ 
+
+  
+    <!-- datatables JS -->
+    <script type="text/javascript" src="vendor/datatables/datatables.min.js"></script>    
+    <!-- código propio JS --> 
+    <script type="text/javascript" src="main.js"></script>  
+    
+    <script src="js/formulario.js"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+    <script src="js/validar_ci_solic.js"></script>
+	<script src="js/validar_ci_enc.js"></script>
+
+</body>
+
+</html>
+
+<!-- Desarrollado por: ANA ANGEL - © Todos los derechos reservados 2022 -->
