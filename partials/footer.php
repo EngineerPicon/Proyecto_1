382e921bffabdfec40c58271415df122aafc0faf
Rolando Picon Nadales <102068223+EngineerPicon@users.noreply.github.com>
2022-07-22 04:28:17 -0400

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,17 @@
+<!-- Footer (inicio de pie de página)-->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+		  <p><b>UNELLEZ</b>
+			La Universidad que Siembra
+			Av. 23 de Enero, Redoma de Ezequiel Zamora, 
+			Sede UNELLEZ - Barinas - Estado Barinas.<br>
+			Email: despachorectoral@unellez.edu.ve
+			RIF: G-20007705-0.</p>
+            <p><address>Desarrollado por: ANA ANGEL 
+			Telefono celular Whatsapp: +584125680489 - 
+			© Todos los derechos reservados 2022</address></p>
+          </div>
+        </div>
+      </footer>
+<!-- End of Footer (fin de pie de página)-->
---
+++ partials/header.php
@@ -0,0 +1,4 @@
+<header><!-- Cabecera de página -->
+	<h3 class="sticky-footer bg-white">Sistema de Control para el Mantenimiento de Consumibles UNELLEZ - VPDS BARINAS</h3>
+</header>
+<!-- Desarrollado por: ANA ANGEL - © Todos los derechos reservados 2022 -->
