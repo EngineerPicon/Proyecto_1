890c7d6193f8943cbe75e2337d1281663706aac5
Rolando Picon Nadales <102068223+EngineerPicon@users.noreply.github.com>
2022-07-22 04:15:58 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="#" />
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="icon" href="Images/favicon.ico"> <!-- Vinculo llamado ico pestaña -->
+	    <title>CTSI - SCMC</title><!-- Título para la pestaña -->
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
+        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">		
+	</head>	
+<body>
+<!-- Código de cabecera de página -->
+ 	<?php include 'partials/header.php' ?>
+<!-- Fin de código cabecera de página -->		
+<!-- Formulario del index.php -->	
+<main>
+	<div class="container-login">
+        <div class="wrap-login">
+           <form><center>
+				<a href="index.php"><img src='./images/logo_peq.jpg'></a> <!--logo de la institución -->
+				<br><br>
+				<table>
+					<td>
+					<a class="boton_iniciosesion" href="sesion.php">Iniciar Sesión</a><br><!-- Boton iniciar sesión -->
+					</td>
+					<td>
+					<a class="boton_iniciosesion" href="signup.php">Registrarse</a><br><!-- Boton registrarse -->	
+					</td>
+				</table>
+			</center>
+		   </form>
+        </div>
+    </div> 			
+<!-- librerias a usar -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
+        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><!-- Salto de lineas -->		
+		<script src="jquery/jquery-3.3.1.min.js"></script>    
+		<script src="bootstrap/js/bootstrap.min.js"></script>    
+		<script src="popper/popper.min.js"></script>    
+		<script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
+		<script src="codigo.js"></script>
+</main>
+<!-- Código de pie de página -->
+ 	<?php include 'partials/footer.php' ?>
+<!-- Fin de código cabecera de página -->
+</body>
+</html>
+<!-- Codigo de pagina de inicio -->
+<!-- Desarrollado por: ANA ANGEL - © Todos los derechos reservados 2022 -->
