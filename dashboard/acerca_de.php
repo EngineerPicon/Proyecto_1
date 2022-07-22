028ad56967b27dd20b606dbf959edd889265ecb0
Rolando Picon Nadales <102068223+EngineerPicon@users.noreply.github.com>
2022-07-22 04:49:43 -0400

    Create acerca_de.php

---
+++ dashboard/acerca_de.php
@@ -0,0 +1,37 @@
+<?php require_once "vistas/parte_superior.php"?> <!-- Para mostrar la parte superior -->
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Acerca de nosotros</h1>
+	
+<p align = "justify"><h2>Misión</h2></p>
+<p align = "justify">Contribuir con las herramientas necesarias para 
+que la población estudiantil de la Universidad 
+Experimental de los Llanos Occidentales 
+"Ezequiel Zamora" UNELLEZ, puedan aplicar y 
+potenciar sus conocimientos en el área de la informática.</p></br>
+
+<p align = "justify"><h2>Visión</h2><p>
+<p align = "justify">Ofrecer un servicio de calidad, 
+que permita el mejoramiento de la actividad docente 
+e investigativa, por otra parte generar un grato 
+ambiente de trabajo donde sus usuarios puedan trabajar 
+a gusto y desarrollar todo su potencial de trabajo 
+en las ciencias de la información.</p></br>
+
+<p align = "justify"><h2>Dirección</h2>
+<p align = "justify">Servir en el día a día, mejorando,
+nuestro sistema te permite obtener resultados a 
+tiempo real y generar informes, gracias al enlace 
+dinámico que tiene, generando Reportes
+ en Formato PDF</p> </br></br>
+
+</body>
+<!-- Desarrollado por: ANA ANGEL - © Todos los derechos reservados 2022 -->
+</html>
+</div>
+<!--FIN del cont principal-->
+
+<?php require_once "vistas/parte_inferior.php"?>
+
+<!-- Desarrollado por: ANA ANGEL - © Todos los derechos reservados 2022 -->
