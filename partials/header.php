5bafca1c13314879a2dbdf16c69d69f151cffc1e
Rolando Picon Nadales <102068223+EngineerPicon@users.noreply.github.com>
2022-07-25 01:43:02 -0400

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,4 @@
 <header><!-- Cabecera de página -->
 	<h3 class="sticky-footer bg-white">Sistema de Control para el Mantenimiento de Consumibles UNELLEZ - VPDS BARINAS</h3>
 </header>
-<!-- Desarrollado por: ANA ANGEL - © Todos los derechos reservados 2022 -->
+<!-- Desarrollado por: Rolando Picon - © Todos los derechos reservados 2022 -->
